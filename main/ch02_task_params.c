f9b60e3adcf950d424c3772b93b3ec405ccb90b5
qlhan <qlhan@incarcloud.com>
2022-04-13 13:28:48 +0800

    0.2.0 task params

--- main/ch02_task_params.c
+++ main/ch02_task_params.c
@@ -5,21 +5,33 @@
 
 static const char *TAG = "ch02_task_params";
 
+int iNum = 100;
+
+typedef struct ComplexNum
+{
+    int a;
+    int b;
+} ComplexNum_t;
+
 void myTask(void *pvParam)
 {
-    int *piNum;
-    piNum = (int *)pvParam;
+    // int *piNum;
+    // piNum = (int *)pvParam;
+    // ESP_LOGI(TAG, "myTask piNum = %d", *piNum);
 
-    ESP_LOGI(TAG, "myTask piNum = %d", *piNum);
+    ComplexNum_t *psNum;
+    psNum = (ComplexNum_t *)pvParam;
+    ESP_LOGI(TAG, "myTask a = %d, b = %d", psNum->a, psNum->b);
 
     vTaskDelay(3000 / portTICK_PERIOD_MS);
 
     vTaskDelete(NULL);
 }
 
-int iNum = 100;
+ComplexNum_t sNum = {1, 2};
 
 void app_main(void)
 {
-    xTaskCreate(myTask, "myTask", 2048, (void *)&iNum, 1, NULL);
+    // xTaskCreate(myTask, "myTask", 2048, (void *)&iNum, 1, NULL);
+    xTaskCreate(myTask, "myTask", 2048, (void *)&sNum, 1, NULL);
 }
