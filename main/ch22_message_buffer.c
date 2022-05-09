e50c454eeecd493aadfcfb7c32ca705ac384477d
qlhan <qlhan@incarcloud.com>
2022-05-09 13:48:36 +0800

    0.10.0 message buffer

---
+++ main/ch22_message_buffer.c
@@ -0,0 +1,121 @@
+#include <stdio.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/message_buffer.h"
+#include "esp_log.h"
+
+static const char *TAG = "ch22_message_buffer.c";
+
+void myTask1(void *pvParam)
+{
+    int i = 0;
+    char pvTxData[50];
+    size_t xTxDataLen = 0;
+    size_t xSendDataLen = 0;
+    MessageBufferHandle_t xMessageBuffer = (MessageBufferHandle_t)pvParam;
+    if (NULL != xMessageBuffer)
+    {
+        ESP_LOGI(TAG, "myTask1 begin, xMessageBuffer is not null");
+    }
+
+    for (;;)
+    {
+        ESP_LOGI(TAG, "myTask1 start");
+
+        // xMessageBufferSend
+        i++;
+        xTxDataLen = sprintf(pvTxData, "No.%d, Hello freeRTOS!", i);
+        xSendDataLen = xMessageBufferSend(xMessageBuffer, (void *)pvTxData, xTxDataLen, portMAX_DELAY);
+
+        ESP_LOGI(TAG, "myTask1 --> pvTxData = %s, xTxDataLen = %d, xSendDataLen = %d", pvTxData, xTxDataLen, xSendDataLen);
+
+        // sleep 1s
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+
+    vTaskDelete(NULL);
+}
+
+void myTask2(void *pvParam)
+{
+    char pvRxData[10];
+    size_t xRecDataLen = 0;
+    MessageBufferHandle_t xMessageBuffer = (MessageBufferHandle_t)pvParam;
+    if (NULL != xMessageBuffer)
+    {
+        ESP_LOGI(TAG, "myTask2 begin, xMessageBuffer is not null");
+    }
+
+    for (;;)
+    {
+        ESP_LOGI(TAG, "myTask2 start");
+
+        // sleep 3s
+        vTaskDelay(pdMS_TO_TICKS(3000));
+
+        // xMessageBufferReceive
+        memset(pvRxData, 0, sizeof(pvRxData));
+        xRecDataLen = xMessageBufferReceive(xMessageBuffer, (void *)pvRxData, sizeof(pvRxData), portMAX_DELAY);
+
+        ESP_LOGI(TAG, "myTask2 --> pvRxData = %s, xRecDataLen = %d", pvRxData, xRecDataLen);
+    }
+
+    vTaskDelete(NULL);
+}
+
+void myTask3(void *pvParam)
+{
+    size_t xCurrSpace = 0;
+    size_t xMinSpace = 1000;
+    MessageBufferHandle_t xMessageBuffer = (MessageBufferHandle_t)pvParam;
+    if (NULL != xMessageBuffer)
+    {
+        ESP_LOGI(TAG, "myTask3 begin, xMessageBuffer is not null");
+    }
+
+    for (;;)
+    {
+        ESP_LOGI(TAG, "myTask3 start");
+
+        // xMessageBufferSpacesAvailable
+        xCurrSpace = xMessageBufferSpacesAvailable(xMessageBuffer);
+        if (xCurrSpace < xMinSpace)
+        {
+            xMinSpace = xCurrSpace;
+        }
+
+        ESP_LOGI(TAG, "myTask3 --> xCurrSpace = %d, xMinSpace = %d", xCurrSpace, xMinSpace);
+
+        // sleep 3s
+        vTaskDelay(pdMS_TO_TICKS(3000));
+    }
+
+    vTaskDelete(NULL);
+}
+
+void app_main(void)
+{
+    // Init
+    MessageBufferHandle_t xMessageBufferHandle = xMessageBufferCreate(1000);
+
+    if (NULL != xMessageBufferHandle)
+    {
+        // vTaskSuspendAll
+        vTaskSuspendAll();
+
+        // xTaskCreate
+        xTaskCreate(myTask1, "myTask1", 1024 * 5, (void *)xMessageBufferHandle, 1, NULL);
+        xTaskCreate(myTask2, "myTask2", 1024 * 5, (void *)xMessageBufferHandle, 1, NULL);
+        xTaskCreate(myTask3, "myTask3", 1024 * 5, (void *)xMessageBufferHandle, 1, NULL);
+
+        // xTaskResumeAll
+        xTaskResumeAll();
+    }
+
+    while (1)
+    {
+        // sleep 5s
+        vTaskDelay(pdMS_TO_TICKS(5000));
+    }
+}
